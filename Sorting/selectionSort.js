985d172270565c8a22d803df213a32ba6f290acc
Nav2513 <134849961+Nav2513@users.noreply.github.com>
2025-03-25 16:02:38 +0530

    Adding Selection sort file

--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -1,1 +1,29 @@
-console.log("Selection sort");
+
+
+
+/*
+        --- Selection Sort ---
+
+Selection Sort is a simple and inefficient comparison-based sorting algorithm. It works by dividing the input array into two parts: a sorted section and an unsorted section. The algorithm repeatedly selects the smallest (or largest, depending on sorting order) element from unsorted section and swaps it with the first unsorted element, expanding the sorted section.
+
+
+Steps of Selection Sort:
+
+1. Start from the first element and assume it is the smallest.
+2. Compare it with all the remaining elements to find the actual smallest element.
+3. Swap the smallest element found with the first element.
+4. Move to the next element and repeat the process for the remaining unsorted section.
+5. Repeat until the entire array is sorted.
+
+
+Time Complexity:
+
+~ Best case: o(n2) (Even if the array is already sorted).
+~ Worst case: o(n2)
+~ Average case: o(n2)
+
+Space Complexity:
+
+~ O(1)
+
+*/
