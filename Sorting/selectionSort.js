41f3066ac475e945e02403ef96ec698b8e472c2e
Nav2513 <134849961+Nav2513@users.noreply.github.com>
2025-03-25 23:21:39 +0530

    Adding selection sort file

--- Sorting/selectionSort.js
+++ Sorting/selectionSort.js
@@ -1,5 +1,30 @@
+function selectionSort(arr) {
+  // Here we are getting array length
+  let length = arr.length;
 
+  /* In the first loop we are loop to whole array by the array length -1,
+        And in the second loop we start loop by i+1, If jth element is smaller than minIndex element
+        then we assign the jth element to minIndex element.
+        In other condition We swap two element if the ith element is smaller than the minIndex than we apply swapping.
+*/
+  for (let i = 0; i < length - 1; i++) {
+    let minIndex = i;
 
+    for (let j = i + 1; j < length; j++) {
+      if (arr[j] < arr[minIndex]) {
+        minIndex = j;
+      }
+    }
+
+    if (minIndex !== i) {
+      [arr[i], arr[minIndex]] = [arr[minIndex], arr[i]];
+    }
+  }
+
+  return arr;
+}
+
+console.log(selectionSort([10, 9, 55, 100, 1, 0]));
 
 /*
         --- Selection Sort ---
