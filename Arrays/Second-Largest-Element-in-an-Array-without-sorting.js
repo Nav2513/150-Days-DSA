6c44b0f5905e39cc01e86e531ad6b9807b4aab4b
Nav2513 <134849961+Nav2513@users.noreply.github.com>
2025-03-29 23:34:28 +0530

    Adding file secondLaregstNumber

---
+++ Arrays/Second-Largest-Element-in-an-Array-without-sorting.js
@@ -0,0 +1,15 @@
+function secondLargestElement(arr) {
+    let n = arr.length;
+    let tempOne = 0;
+    let tempTwo = 0;
+    if(n <= 1){
+        console.log("Array is singleton");
+        return;
+    }
+    for(let i = 0; i < n -1; i++){
+        
+    }
+
+}
+
+console.log(secondLargestElement([20, 52, 14, 100, 79, 42, 53]));
