48a0cc092ea4d5c7998f715bc451d4a668c0e797
Nav2513 <134849961+Nav2513@users.noreply.github.com>
2025-04-01 23:41:12 +0530

    Adding content

--- Arrays/Second-Largest-Element-in-an-Array-without-sorting.js
+++ Arrays/Second-Largest-Element-in-an-Array-without-sorting.js
@@ -1,15 +1,21 @@
-function secondLargestElement(arr) {
+function secondLargestElementInArrayWithoutSorting(arr) {
     let n = arr.length;
-    let tempOne = 0;
-    let tempTwo = 0;
-    if(n <= 1){
-        console.log("Array is singleton");
-        return;
+    let firstLargest;
+    let secondLargest;
+    let upgradeOne;
+    let upgradeTwo;
+    if (n == 1) {
+        console.log("There is no second element exist");
+    } else if (n == 2) {
+        let SecondLargestNumber = Math.min(arr[0], arr[1]);
+        return SecondLargestNumber;
+    } else if (n >= 3) {
+        for (let i = 0; i < n - 1; i++) {
+            firstLargest = Math.max(arr[i], arr[i + 1]);
+            secondLargest = Math.min(arr[i], arr[i + 1]);
+            
+            
+        }
     }
-    for(let i = 0; i < n -1; i++){
-        
-    }
-
 }
-
-console.log(secondLargestElement([20, 52, 14, 100, 79, 42, 53]));
+console.log(secondLargestElementInArrayWithoutSorting([20, 100, 98, 12, 45]));
